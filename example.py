26886f70d0be6233be397425c4fdffe43cda18b5
Anthony Shaw <anthonyshaw@apache.org>
2017-03-24 16:17:58 +1100

    add example

--- example.py
+++ example.py
@@ -1,1 +1,14 @@
 import sys
+
+from saba.client import SabaClient
+from saba.auth.certificate import CertificateAuthentication
+
+host = 'dimensiondatasb-api.sabacloud.com'
+user = sys.argv[1]
+password = sys.argv[2]
+
+cert = CertificateAuthentication(host, user, password)
+
+client = SabaClient(host, cert)
+
+print(client.courses.all())
